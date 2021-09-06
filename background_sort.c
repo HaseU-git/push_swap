d0c8a85f2abc295da41adfaa9040e050d4664e85
HaseU-git <hase.u.git@gmail.com>
2021-09-06 19:20:53 +0900

    Add: add background_sort.c

---
+++ background_sort.c
@@ -0,0 +1,67 @@
+// ### 最終目標
+// バブルソートして、intの配列を作る  
+
+// ### 必要な関数
+
+// - 双方向循環配列を受け取って、intの配列をソートして返す関数  
+//     - 双方向循環リストを受け取ってmallocしたのち、int配列に直して返す関数  
+//     - int配列を受け取って、ソートして返す関数  
+
+// - intの配列を受け取って、その中央値を返す関数  
+
+typedef	struct  s_num_array
+{
+	int *elements;
+    int len;
+}			t_num_array;
+
+
+t_num_array *substitute_for_array(int *array_elements, int array_len)
+{
+    t_num_array array_info;
+    
+    array_info = (t_num_array *)malloc(sizeof(t_num_array));
+    if (!array_info)
+		return NULL;
+    array_info->elements = array_elements;
+    array_info->len = array_len;
+    return array_info;
+}
+
+int count_list_elements(t_node *nil)
+{
+    int list_len;
+    int *unsorted_array;
+    t_node *list_ptr;
+    
+    list_ptr = nil;
+    list_len = 0;
+    while (list_ptr->next != nil)
+    {
+        list_ptr = list_ptr->next;
+        list_len++;
+    }
+    return list_len;
+}
+
+t_num_array *convert_to_array(t_node *nil)
+{
+    int list_len;
+    int *unsorted_array;
+    t_node *list_ptr;
+    int i;
+    
+    list_len = count_list_elements(nil);
+    unsorted_array = (int *)malloc(sizeof(int) * list_len);
+    if (!unsorted_array)
+		return NULL;
+    list_ptr = nil->next;
+    i = 0;
+    while (list_ptr->next != nil)
+    {
+        unsorted_array[i] = list_ptr->key;
+        i++;
+        list_ptr = list_ptr->next;
+    }
+    return (substitute_for_array(list_len, unsorted_array));
+}
