7505cb1e2ac8499b41195a6ef15f66192d883cf4
HaseU-git <hasegawayuta@MacBook-Pro-4.local>
2021-12-10 02:33:16 +0900

    Fix: fix arg check system

--- srcs/arg.c
+++ srcs/arg.c
@@ -1,170 +1,193 @@
 #include "push_swap.h"
+#include <limits.h>
+#include <stdio.h>
 
-int	is_num(char *str)
+int count_digit(int num)
 {
-    int is_num_flag;
-    int i;
+	int digit;
 
-    is_num_flag = 1;
-    i = 0;
-    if (str[i] == '-')
-        i++;
-    else if (str[i] == '0' && str[i + 1] == '\n')
-        return (is_num_flag);
-    while (str[i] != '\0')
-    {
-        if (!('1' <= str[i] && str[i] <= '9'))
-        {
-            is_num_flag = 0;
-            break ;
-        }
-        i++;
-    }
-	return (is_num_flag);
+	digit = 1;
+	num = num / 10;
+	while (num % 10 != 0)
+	{
+		digit = digit + 1;
+		num = num / 10;
+	}
+	return (digit);
 }
 
-int arg_check(int argc, char *argv[])
+int len_str(char *str)
 {
-    int is_error_flag;
-    int i;
+	int len;
 
-    i = 1;
-    is_error_flag = 0;
-    while (i < argc)
-    {
-        if (is_num(argv[i]) == 0)
-        {
-            is_error_flag = 1;
-            break ;
-        }
-        i++;
-    }
-    return is_error_flag;
+	len = 0;
+	if (*str == '-')
+		str = str + 1;
+	while (*str != '\0')
+	{
+		len = len + 1;
+		str = str + 1;
+	}
+	return (len);
 }
 
-int	ft_atoi(const char *str)
+long long str_to_num(char *str)
 {
-	int				i;
-	int				sign;
+	int	i;
+	int	sign;
 	unsigned long	ret;
 
 	i = 0;
 	ret = 0;
 	sign = 1;
-	while ((str[i] >= '\t' && str[i] <= '\r') || str[i] == ' ')
-		i++;
-	if (str[i] == '-' || str[i] == '+')
+	if (str[i] == '-')
 	{
-		if (str[i] == '-')
-			sign *= -1;
+		sign *= -1;
 		i++;
 	}
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		ret *= 10;
-		ret += str[i++] - '0';
-		if (ret > LONG_MAX && sign == 1)
-			return (-1);
-		else if (ret > (unsigned long)LONG_MAX + 1 && sign == -1)
-			return (0);
+		ret += str[i] - '0';
+		i += 1;
 	}
 	return (sign * ret);
 }
 
+int is_int(char *str)
+{
+	int int_flag;
+	int max_digit;
+
+	int_flag = 1;
+	max_digit = count_digit(INT_MAX);
+	if (max_digit < len_str(str))
+		int_flag = 0;
+	else if (str_to_num(str) < INT_MIN)
+		int_flag = 0;
+	else if (INT_MAX < str_to_num(str))
+		int_flag = 0;
+	return int_flag;
+
+}
+
+int	is_num(char *str)
+{
+	int is_num_flag;
+	int i;
+
+	is_num_flag = 1;
+	i = 0;
+	if (str[i] == '-')
+	{
+		i = i + 1;
+		if (!('1' <= str[i] && str[i] <= '9'))
+			is_num_flag = 0;
+
+	}
+	else if ((str[i] == '0' && str[i + 1] != '\0') || str[i] == '\0')
+		is_num_flag = 0;
+	while (str[i] != '\0' && is_num_flag != 0)
+	{
+		if (!('0' <= str[i] && str[i] <= '9'))
+			is_num_flag = 0;
+		i = i + 1;
+	}
+	return (is_num_flag);
+}
+
+int arg_check(int argc, char *argv[])
+{
+	int is_error_flag;
+	int i;
+
+	i = 1;
+	is_error_flag = 0;
+	if (argc == 1)
+		is_error_flag = 1;
+	while (i < argc)
+	{
+		if (is_num(argv[i]) == 0 || is_int(argv[i]) == 0)
+		{
+			is_error_flag = 1;
+			break ;
+		}
+		i++;
+	}
+	return is_error_flag;
+}
+
+
 int is_dupulicated(t_node *list_a)
 {
-    t_node *fixed_node;
-    t_node *variable_node;
-    int is_dup_flag;
+	t_node *fixed_node;
+	t_node *variable_node;
+	int is_dup_flag;
 
-    fixed_node = list_a->next;
-    is_dup_flag = 0;
-    while (fixed_node != list_a)
-    {
-        variable_node = fixed_node->next; 
-        while (variable_node != list_a)
-        {
-            if (fixed_node->key == variable_node->key)
-                is_dup_flag = 1;
-            variable_node = variable_node->next;
-        }
-        fixed_node = fixed_node->next;
-    }
-    return (is_dup_flag);
+	fixed_node = list_a->next;
+	is_dup_flag = 0;
+	while (fixed_node != list_a)
+	{
+		variable_node = fixed_node->next; 
+		while (variable_node != list_a)
+		{
+			if (fixed_node->key == variable_node->key)
+				is_dup_flag = 1;
+			variable_node = variable_node->next;
+		}
+		fixed_node = fixed_node->next;
+	}
+	return (is_dup_flag);
 }
 
-void arg_to_list(t_node *list_a, t_node *list_b, int argc, char *argv[])
-{
-    int i;
-    t_node *new_node;
+// void arg_to_list(t_node *list_a, t_node *list_b, int argc, char *argv[])
+// {
+//     int i;
+//     t_node *new_node;
 
-    i = 1;
-    while (i < argc)
-    {
-        new_node = set_node_last(ft_atoi(argv[i]), list_a);
-        if (new_node == NULL)
-        {
-            free_two_lists(list_a, list_b);
-            write(1, "Error\n", 6);
-            exit(1);
-        }
-        i++;
-    }
+//     i = 1;
+//     while (i < argc)
+//     {
+//         new_node = set_node_last(str_to_num(argv[i]), list_a);
+//         if (new_node == NULL)
+//         {
+//             free_two_lists(list_a, list_b);
+//             write(1, "Error\n", 6);
+//             exit(1);
+//         }
+//         i++;
+//     }
 
-    if (is_dupulicated(list_a))
-    {
-        free_two_lists(list_a, list_b);
-        write(1, "Error\n", 6);
-        exit(1);
-    }
-}
+//     if (is_dupulicated(list_a))
+//     {
+//         free_two_lists(list_a, list_b);
+//         write(1, "Error\n", 6);
+//         exit(1);
+//     }
+// }
 
 #include "push_swap.h"
 
 void show_list(t_node *dummy)
 {
-    t_node *ptr;
-    ptr = dummy->next;
+	t_node *ptr;
+	ptr = dummy->next;
 
-    while (ptr != dummy)
-    {
-        printf("%d\n", ptr->key);
-        ptr = ptr->next;
-    }
+	while (ptr != dummy)
+	{
+		printf("%d\n", ptr->key);
+		ptr = ptr->next;
+	}
 }
 
 int main(int argc, char *argv[])
 {
-    t_node *list_a;
-    t_node *list_b;
+	t_node *list_a;
+	t_node *list_b;
 
-    list_a = init_node(list_a);
-    list_b = init_node(list_b);
+	//     list_a = init_node(list_a);
+	//     list_b = init_node(list_b);
 
-    arg_to_list(list_a, list_b, argc, argv);
-    
-    show_list(list_a);
-    printf("%s", "---------\n");
-    show_list(list_b);
-
-	smaller_half_first_to_second(list_a, list_b);
-	quick_sort(list_a, list_b);
-	larger_half_first_to_second(list_a, list_b);
-	quick_sort(list_a, list_b);
-
-    printf("%s", "---=====--\n");
-    show_list(list_a);
-    printf("%s", "---------\n");
-    show_list(list_b);
-
-    write(1, "Error\n", 7); //7であってる？  
-    // 引数を受け取る  
-    // 引数エラーチェック
-    // 連想配列に格納する(nodeA, nodeBをマロックする) 
-        // mallocのエラーチェック  
-    //  クイックソート（Array情報をmallocする）
-        // 操作系を格納する  
-        // mallocエラーチェック 
-     // 出力する関数  
-    return (1);
+	printf("%d\n", arg_check(argc, argv));
+	return (1);
 }
