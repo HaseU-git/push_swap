16f0bd7f6bc98dcef1266390dc24992f20efcd53
HaseU-git <hasegawayuta@MacBook-Pro-4.local>
2021-12-26 10:12:36 +0900

    Fix: fix norm of free.c

--- srcs/free.c
+++ srcs/free.c
@@ -1,6 +1,6 @@
 #include "push_swap.h"
 
-void put_error()
+void	put_error(void)
 {
 	write(1, "Error\n", 6);
 	exit(0);
@@ -13,10 +13,10 @@ void	free_error_exit(t_node *list_a, t_node *list_b, t_prs *process)
 	put_error();
 }
 
-void    free_all_nodes(t_node *dummy)
+void	free_all_nodes(t_node *dummy)
 {
-	t_node    *ptr;
-	t_node    *tmp;
+	t_node	*ptr;
+	t_node	*tmp;
 
 	ptr = dummy->next;
 	while (ptr != dummy)
@@ -29,11 +29,10 @@ void    free_all_nodes(t_node *dummy)
 	return ;
 }
 
-
-void    free_all_process(t_prs *dummy)
+void	free_all_process(t_prs *dummy)
 {
-	t_prs    *ptr;
-	t_prs    *tmp;
+	t_prs	*ptr;
+	t_prs	*tmp;
 
 	ptr = dummy->next;
 	while (ptr != dummy)
@@ -46,7 +45,7 @@ void    free_all_process(t_prs *dummy)
 	return ;
 }
 
-void    free_two_lists(t_node *list_a, t_node *list_b)
+void	free_two_lists(t_node *list_a, t_node *list_b)
 {
 	free_all_nodes(list_a);
 	free_all_nodes(list_b);
